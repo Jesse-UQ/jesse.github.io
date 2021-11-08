f245c403a105232a92299e7d0d0018ea24ac4f72
Jesse-UQ <jesse20au@gmail.com>
2021-11-08 18:08:38 +1100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+      $(".goTop").fadeIn();
+    } else {
+      $(".navbar").removeClass("sticky");
+      $(".goTop").fadeOut();
+    }
+  });
+
+  $(".menu-toggler").click(function () {
+    $(this).toggleClass("active");
+    $(".navbar-menu").toggleClass("active");
+  });
+
+  $(".goTop").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      200
+    );
+  });
+});
